2d93d9b2c35dcf2db1ec2330287118f7ed47aaa4
ZheniaKarpovich <zhenia.karpovich@gmail.com>
2017-09-24 22:07:15 +0300

    My first task,guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() 
+    {
+        this.number=number;
+    }
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
     }
 
     guess() {
-
+        return Math.round((this.max-this.min)/2);
     }
 
     lower() {
-
+        this.max=Math.round(this.max/2);
     }
 
     greater() {
-
+        this.min= Math.round((this.max-this.min)/2);
     }
 }
 
